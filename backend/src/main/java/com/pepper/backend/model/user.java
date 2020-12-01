113eec15d2b24c9dd940b85cc063a0d66a6e6a8f
GregWasz <gregory.waszkow@icloud.com>
2020-12-01 22:55:33 +0100

    Added relations to other models

--- backend/src/main/java/com/pepper/backend/model/user.java
+++ backend/src/main/java/com/pepper/backend/model/user.java
@@ -2,18 +2,27 @@ package com.pepper.backend.model;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Set;
 
 @Data
 @Entity
-public class user {
+@Table(name = "users")
+public class User {
     @Id
-    @GeneratedValue
-    private int user_id;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long user_id;
     private String user_name;
     private String password;
     private String email;
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "role_id", referencedColumnName = "role_id")
+    private Role role;
+
+    @OneToMany(mappedBy="user")
+    private Set<Post> posts;
+
+    @OneToMany(mappedBy="user")
+    private Set<Comment> UserComments;
 }
 
