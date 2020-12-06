b138b486a2b6656a5e783d2d617fc98d46650d1f
GregWasz <gregory.waszkow@icloud.com>
2020-12-06 23:11:06 +0100

    Spring Security connected with database

--- backend/src/main/java/com/pepper/backend/model/User.java
+++ backend/src/main/java/com/pepper/backend/model/User.java
@@ -1,14 +1,20 @@
 package com.pepper.backend.model;
 
 import lombok.Data;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Set;
 
 @Data
 @Entity
 @Table(name = "users")
-public class User{
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name="userId")
@@ -25,4 +31,16 @@ public class User{
 
     @OneToMany(mappedBy="user")
     private Set<Comment> UserComments;
+
+    @CreatedBy
+    private String createdBy;
+    private boolean accountNonExpired = true;
+    private boolean accountNonLocked = true;
+    private boolean credentialsNonExpired = true;
+    private boolean enabled = true;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.asList(new SimpleGrantedAuthority(getRole().getRoleName()));
+    }
 }
--- backend/src/main/java/com/pepper/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/pepper/backend/security/SecurityConfig.java
@@ -3,31 +3,32 @@ package com.pepper.backend.security;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity(debug = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    //private PasswordEncoder passwordEncoder;
-
     @Qualifier("myUserDetailsService")
     @Autowired
     UserDetailsService userDetailsService;
 
+    private PasswordEncoder passwordEncoder;
 
+    public SecurityConfig(PasswordEncoder passwordEncoder, @Qualifier("myUserDetailsService") UserDetailsService userDetailsService) {
+        this.passwordEncoder = passwordEncoder;
+        this.userDetailsService = userDetailsService;
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
     }
 
     @Override
@@ -46,11 +47,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf()
                 .disable();
     }
-
-    @Bean
-    public PasswordEncoder getPasswordEncoder() {return NoOpPasswordEncoder.getInstance();
-    }
-
-
-
 }
