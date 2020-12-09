2797ed561beef45409da1893a0ce9f13585df0b3
GregWasz <gregory.waszkow@icloud.com>
2020-12-09 02:20:41 +0100

    Refactor

--- backend/src/main/java/com/pepper/backend/repository/RoleRepository.java
+++ backend/src/main/java/com/pepper/backend/repository/RoleRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Integer> {
-    Optional<Role>  findByRoleName(String name);
+    Optional<Role> findByRoleName(String name);
 }
