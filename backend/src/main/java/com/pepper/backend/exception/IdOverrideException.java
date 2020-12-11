95907251b4fa950ca4bffbdb2933de177e0637e2
GregWasz <gregory.waszkow@icloud.com>
2020-12-11 01:57:13 +0100

    - Added response status to exceptions

--- backend/src/main/java/com/pepper/backend/exception/IdOverrideException.java
+++ backend/src/main/java/com/pepper/backend/exception/IdOverrideException.java
@@ -1,5 +1,9 @@
 package com.pepper.backend.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
 public class IdOverrideException extends RuntimeException{
     private static final long serialVersionUID = 1L;
     public IdOverrideException(String message){
--- backend/src/main/java/com/pepper/backend/exception/UnauthenticatedException.java
+++ backend/src/main/java/com/pepper/backend/exception/UnauthenticatedException.java
@@ -1,5 +1,9 @@
 package com.pepper.backend.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
 public class UnauthenticatedException extends RuntimeException{
     private static final long serialVersionUID = 1L;
     public UnauthenticatedException(String message){
