919cdcbf08acc90201addd7face2791e5220e052
GregWasz <gregory.waszkow@icloud.com>
2020-12-12 15:25:22 +0100

    - Add MessageDto

---
+++ backend/src/main/java/com/pepper/backend/dto/MessageDto.java
@@ -0,0 +1,10 @@
+package com.pepper.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class MessageDto {
+    public final String message;
+}
