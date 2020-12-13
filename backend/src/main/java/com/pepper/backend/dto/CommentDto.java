c21ccd60a5096eba94c8644490ea4a30be3a51fa
GregWasz <gregory.waszkow@icloud.com>
2020-12-13 17:40:33 +0100

    - Added CommentDto

---
+++ backend/src/main/java/com/pepper/backend/dto/CommentDto.java
@@ -0,0 +1,10 @@
+package com.pepper.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CommentDto {
+    private final String content;
+}
